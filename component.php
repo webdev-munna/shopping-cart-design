6b538161aa0ee663160eb8b1b7c461e981b824b6
webdev_munna <112567430+webdev-munna@users.noreply.github.com>
2023-02-13 01:08:46 +0600

    changed color on component.php

--- component.php
+++ component.php
@@ -3,7 +3,7 @@
 function component($props)
 {
   $element = "
-         <h2 style='color:red; text-align:center; margin-top:10px'>$props</h2>
+         <h2 style='color:green; text-align:center; margin-top:10px'>$props</h2>
     ";
   echo $element;
 }
