890a21983b7f48896693592429e57c28bae3f92c
webdev_munna <112567430+webdev-munna@users.noreply.github.com>
2023-02-12 21:49:08 +0600

    changed serverName variable to host on connect-db.php

--- connect-db.php
+++ connect-db.php
@@ -1,11 +1,11 @@
 <?php
 
-$serverName = "localhost";
+$host = "localhost";
 $userName = "root";
 $password = "";
 $dbName = "shop";
 
-$conn = mysqli_connect($serverName, $userName, $password, $dbName);
+$conn = mysqli_connect($host, $userName, $password, $dbName);
 if (!$conn) {
   echo "Connecttion failed:" . mysqli_connect_error($conn);
 } else {
